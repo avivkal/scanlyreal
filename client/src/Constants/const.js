103efeb57a3f2b937aba7a90e448d84129458022
Aviv Kalmanson <avivkalmanson@gmail.com>
2020-12-20 11:22:58 +0200

    Update const.js

--- client/src/Constants/const.js
+++ client/src/Constants/const.js
@@ -6,8 +6,8 @@ export const WIFI_SETUP_PATH = '/wifi';
 export const DASHBOARD_NOT_ADDED_PATH = '/notAdded'
 export const DASHBOARD_FILTERS_PATH = '/filters'
 export const HOME_PAGE_PATH = '/home'
-export const URL = 'http://localhost:5000';
-// export const URL = 'http://scanly.net';
+// export const URL = 'http://localhost:5000';
+export const URL = 'http://scanly.net';
 export const SHUFERSAL = "shufersal";
 export const RAMI_LEVY = "ramiLevy";
 
